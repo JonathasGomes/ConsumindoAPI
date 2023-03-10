1c31370f800b4e332e96ee8c83d182d213d84988
JonathasGomes <jonathas553@hotmail.com>
2023-03-10 09:11:10 -0300

    Initial Commit

---
+++ ViaCep/Cep.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViaCep
+{
+    public class Cep
+    {
+        private readonly RestClient _restClient;
+
+        public Cep()
+        {
+            const string cepApiBaseUrl = "https://viacep.com.br/";
+            _restClient = new RestClient(cepApiBaseUrl);
+        }
+
+        string endpoint;
+
+        public void ConsultarCEP()
+        {
+            Console.WriteLine("Digite o CEP");
+            endpoint = Console.ReadLine();
+        }
+
+        public ViaCepModel GetCeps()
+        {
+            var request = new RestRequest(resource: $"ws/{endpoint}/json/");
+
+            string cepsJson = _restClient.Get(request).Content;
+            
+            ViaCepModel ceps = JsonConvert.DeserializeObject<ViaCepModel>(cepsJson);
+
+            return ceps;
+        }
+
+    }
+}
---
+++ ViaCep/Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using ViaCep;
+
+var viacep = new Cep();
+
+viacep.ConsultarCEP();
+
+var cep = viacep.GetCeps();
+
+
+Console.WriteLine(cep.Localidade);
+Console.WriteLine(cep.Complemento);
+Console.WriteLine(cep.Bairro);
+Console.WriteLine(cep.Uf);
+Console.WriteLine(cep.Ibge);
+Console.WriteLine(cep.Gia);
